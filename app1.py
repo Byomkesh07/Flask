105c8a172926a42fd9f490ea168588ae4be2d5d6
Byomkesh <byomkeshbhattacharjee2000@gmail.com>
2024-08-08 18:02:34 +0530

    Updated

--- app1.py
+++ app1.py
@@ -23,6 +23,8 @@ def results(marks):
         result = 'success'
     return redirect(url_for(result,score=marks))
 
+## redirect takes a URL or endpoint as its argument and sends a response that tells the browser to navigate to that URL.
+## url_for generates a URL for the endpoint named result, which will be either 'success' or 'fail', and includes any parameters (in this case, score=marks) that are needed to build the URL.
 
 if __name__ == '__main__':
     app1.run(debug=True)   
