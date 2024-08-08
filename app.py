06da6f6f67f38430d87139000249a186979fc01d
Byomkesh <byomkeshbhattacharjee2000@gmail.com>
2024-08-08 15:18:23 +0530

    First flask application

--- app.py
+++ app.py
@@ -1,2 +1,16 @@
-msg=("Welcome to Flask")
-print(msg)
+from flask import Flask
+
+## WSGI (Web Server Gateway Interface) Application
+
+app = Flask(__name__)
+
+@app.route('/')
+def welcome():
+    return "Welcome to my first web application"
+
+@app.route('/members')
+def members():
+    return "Welcome to my first web applications member page"
+
+if __name__ == '__main__':
+    app.run(debug=True)
